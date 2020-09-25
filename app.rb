9ea4eb9faac092174300d0eccc53f1de919a09d4
Owen Moss <owenkmoss@gmail.com>
2020-09-25 08:19:20 +0200

    added cookbook and recipe, views and bootstrap not finisehd yet

--- app.rb
+++ app.rb
@@ -2,11 +2,27 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  '<h1>Hello <em>world</em>!</h1>'
+  erb :index
 end
+
+# get '/' do
+#   @usernames = [ 'ssaunier', 'Papillard' ]
+#   erb :index
+# end
+
+# get '/about' do
+#   erb :about
+# end
+
+# get '/team/:username' do
+#   binding.pry
+#   puts params[:username]
+#   "The username is #{params[:username]}"
+# end
